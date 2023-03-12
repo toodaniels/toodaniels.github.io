ed8412506000a127aa6923de5536287706f36a1b
toodaniels <torresestradadaniel@gmail.com>
2023-03-12 04:18:23 +0000

    Deploying to public from @ toodaniels/toodaniels.github.io@80bead75d6a96870a864cfa24c69e010cd83ab96 🚀

---
+++ _next/static/vK61RJl_MMCXfMBhZCgyB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a2870ef2425cf3cc.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
